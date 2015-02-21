be66ae7b46c2da80727323602542c15c133f2f16
KimSia Sim <kimcity@gmail.com>
2015-02-21 16:45:08 +0800

    create custom admin action for reports

--- weasyprint_site/reports/admin.py
+++ weasyprint_site/reports/admin.py
@@ -3,12 +3,20 @@ from django.contrib import admin
 # Register your models here.
 from reports.models import Report, ReportTemplate
 
+def print_as_pdf(modeladmin, request, queryset):
+	return
+
+print_as_pdf.short_description = "Generate pdf"
+
 class ReportAdmin(admin.ModelAdmin):
     fields = ['commodity', 
     'date',
     'trade_period',
     'quantity_cutoff',
+    'data',
+    'template',
     'title']
+    actions = [print_as_pdf]
 
 admin.site.register(Report, ReportAdmin)
 admin.site.register(ReportTemplate)
--- weasyprint_site/reports/models.py
+++ weasyprint_site/reports/models.py
@@ -19,12 +19,10 @@ class Report(models.Model):
 	trade_period = models.CharField(max_length=10, default='open')
 	quantity_cutoff = models.IntegerField(default=0)
 	printed = models.BooleanField(default=0)
-	datetime_email_sent = models.DateTimeField('date email sent')
+	datetime_email_sent = models.DateTimeField('date email sent', blank=True, null=True)
 	data = models.TextField(default = 'data here...')
 	template = models.ForeignKey(ReportTemplate)
 	# __unicode__ on Python 2
 	# __str__ on Python 3
 	def __unicode__(self):
 		return self.title
-
-		
